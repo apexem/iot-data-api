23255dc08d0ce438ff62ecbaa64b696184d6747d
apexen <apexem@users.noreply.github.com>
2019-12-17 23:54:41 +0100

    updated esp json schemas

--- Models/EspJsons/ESPJsons.cs
+++ Models/EspJsons/ESPJsons.cs
@@ -3,10 +3,7 @@ namespace sql_berry_api.Models
 {
     public abstract class ESPJson
     {
-        public string id { get; set; }
-        public string name { get; set; }
-        public string hardware { get; set; }
-        public bool connected { get; set; }
+        public string module { get; set; } 
     }
 
     public class TemperatureJson : ESPJson
